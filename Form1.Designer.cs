54887eedee25d3d196d1920daadb52cf2ac8f89a
jiyeong1004 <ak1157@naver.com>
2021-04-02 22:44:45 +0900

    버튼 추가 및 버튼 클릭 핸들러 코드 작성

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,7 +39,7 @@ namespace HelloWinform
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(262, 179);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "클릭하세요!!!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -49,8 +49,9 @@ namespace HelloWinform
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(262, 172);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "나도 버튼";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
